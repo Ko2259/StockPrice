f149af2936baddec8bc10376d89b2848dd54c5c6
Shuntao Chen <shuntao@uw.edu>
2022-11-30 19:52:01 -0800

    temp

--- StockPrice/1.py
+++ StockPrice/1.py
@@ -1,1 +1,12 @@
+'''
 from src.StockPrice_visualize import StockData
+from src.StockPrice_ML import StockPrediction
+data = StockData(["Meta"], "2021-01-01", "2022-10-30")
+
+model = StockPrediction(data = data)
+
+
+pred, interval = model.predict(days = 5)
+print(pred.index)
+'''
+print(5*1000*1000*1000/(1024**3))
