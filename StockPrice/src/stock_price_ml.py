4a2a6c58cf909edd85e3c68d6eaf65c68e49d14e
Shuntao Chen <shuntao@uw.edu>
2022-12-07 16:35:20 -0800

    with evaluation

--- StockPrice/src/stock_price_ml.py
+++ StockPrice/src/stock_price_ml.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from pandas_datareader import data
+from pandas_datareader.data import DataReader
 import datetime as dt
 import pandas_market_calendars as mcal
 import plotly.express as px
@@ -25,23 +25,30 @@ class StockPrediction:
         self.train, self.y = {}, {}
         self.model = {}
         self.pred = {}
-        self.date = {}
+        self.val = val
 
         for stock in self.stocks:
-            self.train[stock], self.y[stock] = self.get_train_y(self.data.df[stock], val)
+            self.train[stock], self.y[stock] = self.get_train_y(self.data.df[stock])
             self.model[stock] = self.build_model(method)
             self.model[stock].fit(self.y[stock])
             self.pred[stock] = pd.DataFrame()
-            self.date[stock] = self.train[stock]["Date"].iloc[-1]
+            self.date_pred = self.train[stock]["Date"].iloc[-1]
+            self.date_train = self.date_pred
 
+    def check_day_open(self, date):
+        nyse = mcal.get_calendar("NYSE")
+        start, end = date - pd.Timedelta(days = 5), date + pd.Timedelta(days = 5)
+        market_days = nyse.valid_days(start, end)
 
-    def get_train_y(self, df, val):
-        train = df.reset_index()[["Date", val]]
-        y = train[val]
+        return date.tz_localize("UTC") in market_days 
+
+    def get_train_y(self, df):
+        train = df.reset_index()[["Date", self.val]]
+        y = train[self.val]
         return train, y
 
     def build_model(self, method):
-        forecaster = TBATS(use_box_cox=True, use_trend=True, use_arma_errors=True)
+        forecaster = TBATS(use_box_cox=True, use_trend=True, use_arma_errors=True, sp = 5)
         return forecaster
 
     def predict(self, days = 1, level = 0.95):
@@ -64,16 +71,56 @@ class StockPrediction:
 
             pred[stock] = pd.DataFrame({stock : pred_val, stock + "-low" : low,
                                         stock + "-high": high}, index = index)
-            if index[0] > self.date[stock]:
-                pass ### update the self.pred and self.date
-            
+            if index[0] > self.date_pred:
+                #update
+                pred_tomorrow = pd.DataFrame({stock : pred_val[:1], stock + "-low" : low[:1],
+                                        stock + "-high": high[:1]}, index = index[:1])
+                self.pred[stock] = pd.concat([self.pred[stock], pred_tomorrow])
+
+        self.date_pred = index[0]    
         return pred
 
-    '''
-    def update(date = ""):
-        ## throw value error if date is not today
-        if date = 
-    '''   
+    def update(self, date = None, message = True):
+        if not date:
+            date = pd.Timestamp(pd.Timestamp.now().date()) ## take to be today
+        else:
+            date = pd.Timestamp(date)
+
+        if date > pd.Timestamp.now():
+            raise ValueError("Given date is after today.")
+        
+        if not self.check_day_open(date):
+            if message:
+                print(f"The Stock Market if not open on {date.strftime('%Y-%m-%d')}"
+                    +", no update needed.")
+            return
+
+        if self.date_train >= date:
+            if message:
+                print(f"The day {date.strftime('%Y-%m-%d')} is already contained in the model, "
+                    +"no update needed")
+            return
+
+        expire_after = dt.timedelta(days=3)
+        session = requests_cache.CachedSession(cache_name='cache', expire_after=expire_after)
+        session.headers = DEFAULT_HEADERS
+        ### update model, train, y
+        for stock in self.stocks:
+            data = DataReader(stock, 'yahoo', self.date_train+pd.Timedelta(days = 1), date)
+            train, y = self.get_train_y(data)
+            self.train[stock] = pd.concat([self.train[stock], train]).reset_index(drop = True)
+            self.y[stock] = pd.concat([self.y[stock], y]).reset_index(drop = True)
+            self.model[stock].update(y = y)
+        self.date_train = date
+
+        return
+
+
+
+        
+
+
+
 
 
 
---
+++ StockPrice/tests/test_ML.py
@@ -0,0 +1,27 @@
+import numpy as np
+import unittest
+import pandas as pd
+from pandas_datareader import data as web
+from datetime import datetime
+from pandas_datareader._testing import skip_on_exception
+from pandas_datareader._utils import RemoteDataError
+
+from src.stock_price_visualize import StockData
+from src.stock_price_ml import StockPrediction
+
+class TestVisual(unittest.TestCase):
+
+	def test_smoke(self):
+		'''
+		Smoke test, see whether the class works
+		'''
+		start_time = "2022-01-05"
+		end_time = "2022-10-10"
+		data = StockData(["Meta"], start_time, end_time)
+		model = StockPrediction(data)
+		return
+
+if __name__ == "__main__":
+	unittest.main()
+	
+
